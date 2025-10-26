7c59e10bc7519226ebeafc7a8b38f24a7c493a38
Manish Gupta <mk.kumar666888@gmail.com>
2025-10-26 14:06:28 +0530

    week4 Probability

---
+++ week4 Probability/Day1/samlpe/sample.py
@@ -0,0 +1,12 @@
+# Basic Probability Concepts 
+
+# . Sample Sapce andEvents 
+# Sample Space: The set of all possible outcomes of a rendom expriment
+# Events : A subset of the sample space 
+
+    # Conditional Probability 
+# . The probability of an event A occurring, given that B has occurred is denoted as P(A|B)
+
+  # independent 
+  # Two events A and B are independet if 
+    # p(A/B) = P(A) and P(B)
